57532b85c6a07e8269200b973235e0ecc598d8bb
PraveenRepo <praveen.intelegencia@gmail.com>
2018-07-29 20:07:09 +0530

    Parameter concept in TestNG

---
+++ src/com/parameter/testng/ParametersTest.java
@@ -0,0 +1,51 @@
+package com.parameter.testng;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParametersTest {
+	
+	WebDriver driver;
+	
+	@Parameters({"env","url","uname","pass","browser"})
+	@Test
+	public void loginTest(String env,String url,String uname,String pass,String browser)
+	{
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get(url);
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys(uname);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass);
+		
+		driver.findElement(By.xpath("//input[@type='submit']")).submit();
+		
+		String title= driver.getTitle();
+		System.out.println("Ttile is: "+title);
+		Assert.assertEquals(title, "CRMPRO");
+		
+		driver.quit();
+	}
+
+}
